6bc9e303f7d777676c57db08458c9f204063f4e2
Ehsan Ziya <ziyaeddiny@gmail.com>
2015-09-23 16:54:19 +0100

    Refactor: Clean up unused code

--- build/demo.js
+++ build/demo.js
@@ -14,38 +14,18 @@ var scene = new Scene('eu', beet, {
   freq: 440
 });
 
-// var scene2 = new Scene('eu', beet, {
-//   slots: 3,
-//   pulses: 3,
-//   freq: 220
-// });
-
-var scene3 = new Scene('eu', beet, {
+var scene2 = new Scene('eu', beet, {
   slots: 5,
   pulses: 5,
   freq: 220
 });
 
-// setTimeout(function() {
-//   scene.change({
-//     slots: 4,
-//     pulses: 4
-//   });
-//   setTimeout(function() {
-//     scene.change({
-//       slots: 19,
-//       pulses: 7
-//     });
-//   }, 3000);
-// }, 3000);
-
 beet.start();
 
 function animate() {
   requestAnimationFrame(animate);
-  // scene2.render();
   scene.render();
-  scene3.render();
+  scene2.render();
 }
 
 animate();
@@ -239,10 +219,6 @@ Scene.prototype.change = function(options) {
   var self = this;
   self.pattern.update(options.pulses, options.slots);
   self._addPoints();
-};
-
-Scene.prototype.audioCallback = function(time, step, drawTime) {
-
 };
 
 module.exports = Scene;
--- demo.js
+++ demo.js
@@ -13,38 +13,18 @@ var scene = new Scene('eu', beet, {
   freq: 440
 });
 
-// var scene2 = new Scene('eu', beet, {
-//   slots: 3,
-//   pulses: 3,
-//   freq: 220
-// });
-
-var scene3 = new Scene('eu', beet, {
+var scene2 = new Scene('eu', beet, {
   slots: 5,
   pulses: 5,
   freq: 220
 });
 
-// setTimeout(function() {
-//   scene.change({
-//     slots: 4,
-//     pulses: 4
-//   });
-//   setTimeout(function() {
-//     scene.change({
-//       slots: 19,
-//       pulses: 7
-//     });
-//   }, 3000);
-// }, 3000);
-
 beet.start();
 
 function animate() {
   requestAnimationFrame(animate);
-  // scene2.render();
   scene.render();
-  scene3.render();
+  scene2.render();
 }
 
 animate();
