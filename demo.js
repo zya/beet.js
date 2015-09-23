1081bcccb2a5d2b6e371dd05be7509173c65627b
Ehsan Ziya <ziyaeddiny@gmail.com>
2015-09-23 16:00:00 +0100

    Feature: Add ability to change the visuals in the demo

--- demo.js
+++ demo.js
@@ -1,8 +1,29 @@
 var dat = require('dat-gui');
 
 var Scene = require('./lib/demo/scene');
+var Beet = require('./index');
+var context = new AudioContext();
 
-var scene = new Scene('eu', {}, {});
+var beet = new Beet({
+  context: context
+});
+var scene = new Scene('eu', beet, {
+  slots: 9,
+  pulses: 2
+});
+
+setTimeout(function() {
+  scene.change({
+    slots: 4,
+    pulses: 4
+  });
+  setTimeout(function() {
+    scene.change({
+      slots: 19,
+      pulses: 7
+    });
+  }, 1000);
+}, 1000);
 
 function animate() {
   requestAnimationFrame(animate);
